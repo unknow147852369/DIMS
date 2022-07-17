1e5fb62c4475773770812e700883a1d9b411a1a0
unknow147852369 <79589847+unknow147852369@users.noreply.github.com>
2022-07-17 19:15:05 +0700

    17/7

--- DIMSApi/DIMSApis/DIMSApis/Models/Output/RoomDetailInfoPeopleOutput.cs
+++ DIMSApi/DIMSApis/DIMSApis/Models/Output/RoomDetailInfoPeopleOutput.cs
@@ -5,8 +5,10 @@
         public int InboundUserId { get; set; }
         public int? BookingId { get; set; }
         public string? UserName { get; set; }
+        public string? UserSex { get; set; }
         public string? UserIdCard { get; set; }
         public DateTime? UserBirthday { get; set; }
+        public string? UserAddress { get; set; }
         public int? Status { get; set; }
     }
 }
