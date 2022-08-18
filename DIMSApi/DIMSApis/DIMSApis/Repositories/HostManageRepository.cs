0cd0945b3c2456d3e0006ea2dfe17fa67b90776f
unknow147852369 <79589847+unknow147852369@users.noreply.github.com>
2022-08-18 13:52:17 +0700

    18/8

--- DIMSApi/DIMSApis/DIMSApis/Repositories/HostManageRepository.cs
+++ DIMSApi/DIMSApis/DIMSApis/Repositories/HostManageRepository.cs
@@ -129,14 +129,15 @@ namespace DIMSApis.Repositories
         public async Task<RoomDetailInfoOutput> GetADetailRoom(int userId, int RoomId, DateTime today)
         {
             var RoomDetail = await _context.Rooms.Where(op => op.RoomId == RoomId)
-                .Include(bd => bd.BookingDetails.Where(op => op.StartDate.Value.Date <= today.Date && op.EndDate.Value.Date >= today.Date && op.Status.Value)).ThenInclude(b => b.Booking).ThenInclude(ib => ib.InboundUsers)
-                .Include(bd => bd.BookingDetails.Where(op => op.StartDate.Value.Date <= today.Date && op.EndDate.Value.Date >= today.Date && op.Status.Value)).ThenInclude(b => b.Booking).ThenInclude(u => u.User)
+                .Include(bd => bd.BookingDetails.Where(op => (op.StartDate.Value.Date <= today.Date && op.EndDate.Value.Date >= today.Date)||(op.Booking.QrCheckUp.CheckOut == null)  && op.Status.Value)).ThenInclude(b => b.Booking).ThenInclude(ib => ib.InboundUsers)
+                .Include(bd => bd.BookingDetails.Where(op => (op.StartDate.Value.Date <= today.Date && op.EndDate.Value.Date >= today.Date) || (op.Booking.QrCheckUp.CheckOut == null) && op.Status.Value)).ThenInclude(b => b.Booking).ThenInclude(u => u.User)
                 .Include(c => c.Category)
                 .SingleOrDefaultAsync()
             ;
 
             if (RoomDetail == null) { return null; }
             var returnResult = _mapper.Map<RoomDetailInfoOutput>(RoomDetail);
+            returnResult.EndDate = returnResult.EndDate.Value.AddDays(1).Add(new TimeSpan(12, 00, 0));
             return returnResult;
         }
 
